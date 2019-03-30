0a65d13fdc830063dc94b34f2b1e8d188baac2c2
ragnar-l <kleiner.matias@gmail.com>
2019-03-30 19:37:39 -0300

    login y envio de archivos

--- TP1/codigo/cliente.c
+++ TP1/codigo/cliente.c
@@ -1,29 +1,36 @@
-/* Cliente en el dominio Unix - orientado a corrientes */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <string.h>
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
-#define TAM 80
+#define TAM 1024
+
+static char hostname[TAM];
+static char user[TAM];
 
 int loggin(int sockfd);
+void func_detect(int sockfd);
+void send_to_server(int sockfd);
+int update_firmware(int sockfd);
 
 int main(int argc, char *argv[])
 {
 	int sockfd, servlen;
 	struct sockaddr_un serv_addr;
-	int terminar = 0;
+	int control_error;
+	char buffer[TAM];
 
 	if (argc < 2)
 	{
@@ -48,32 +55,219 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	/** 
+	 * Llamo a la funcion log. 
+	**/
+	control_error = loggin(sockfd);
+
+	if (control_error == 1)
+	{
+
+		memset(hostname, '\0', sizeof(hostname));
+		memset(buffer, '\0', sizeof(buffer));
+		control_error = read(sockfd, buffer, sizeof(buffer));
+		if (control_error < 0)
+		{
+			perror("lectura de socket");
+			exit(1);
+		}
+		strcpy(hostname, "[");
+		strcat(hostname, ANSI_COLOR_YELLOW);
+		strcat(hostname, user);
+		strcat(hostname, buffer);
+		strcat(hostname, ANSI_COLOR_RESET);
+		strcat(hostname, "] ");
+	}
+
+	else
+	{
+		printf("Error durante el log, cierro el cliente.\n");
+		exit(0);
+	}
+
 	while (1)
 	{
-		int prueba;
-		prueba = loggin(sockfd);
 
-		if (prueba == 1) {
-			printf("SEEEEEEEEEEEEEEEE");
-		}
-		if (terminar)
-		{
-			printf("Finalizando ejecución\n");
-			exit(0);
-		}
+		printf(hostname);
+		fflush(stdout);
+		send_to_server(sockfd);
+		func_detect(sockfd);
 	}
 	return 0;
 }
 
+void send_to_server(int sockfd)
+{
+	static char buffer[TAM];
+	static int control_error;
+	memset(buffer, '\0', TAM);
+	fgets(buffer, TAM - 1, stdin);
+	control_error = write(sockfd, buffer, sizeof(buffer));
+	if (control_error < 0)
+	{
+		printf("Error escribir en socket\n");
+		exit(0);
+	}
+}
+
+void func_detect(int sockfd)
+{
+	static char buffr[TAM];
+	static int control_error;
+	memset(buffr, '\0', TAM);
+	control_error = read(sockfd, buffr, TAM);
+	if (control_error < 0)
+	{
+		perror("lectura de socket");
+		exit(1);
+	}
+	if (strcmp(buffr, "recibi update") == 0)
+	{
+		update_firmware(sockfd);
+		fflush(stdout);
+	}
+	else if (strcmp(buffr, "recibi start scanning") == 0)
+	{
+	}
+
+	else if (strcmp(buffr, "recibi obtener telemetria") == 0)
+	{
+	}
+}
+
+int update_firmware(int sockfd)
+{
+	// Start function
+
+	int recv_size = 0, size = 0, read_size, write_size, packet_index = 1, stat;
+
+	char imagearray[10241];
+	FILE *image;
+
+	printf("NADAAAA");
+	fflush(stdout);
+	
+	
+	//Find the size of the image
+	
+	stat = read(sockfd, &size, sizeof(size));
+	
+
+	printf("Packet received.\n");
+	printf("Packet size: %i\n", stat);
+	printf("Image size: %i\n", size);
+	printf(" \n");
+	fflush(stdout);
+
+	
+
+	//Send our verification signal
+	do
+	{
+		stat = write(sockfd, &size, sizeof(size));
+	} while (stat < 0);
+
+	printf("Reply sent\n");
+	printf(" \n");
+
+	image = fopen("file1.txt", "w");
+
+	if (image == NULL)
+	{
+		printf("Error has occurred. Image file could not be opened\n");
+		return -1;
+	}
+
+	//Loop while we have not received the entire file yet
+
+	struct timeval timeout = {10, 0};
+
+	fd_set fds;
+	int buffer_fd;
+
+	while (recv_size < size)
+	{
+		//while(packet_index < 2){
+
+		FD_ZERO(&fds);
+		FD_SET(sockfd, &fds);
+
+		buffer_fd = select(FD_SETSIZE, &fds, NULL, NULL, &timeout);
+
+		if (buffer_fd < 0)
+			printf("error: bad file descriptor set.\n");
+
+		if (buffer_fd == 0)
+			printf("error: buffer read timeout expired.\n");
+
+		if (buffer_fd > 0)
+		{
+			do
+			{
+				read_size = read(sockfd, imagearray, 10241);
+			} while (read_size < 0);
+
+			printf("Packet number received: %i\n", packet_index);
+			printf("Packet size: %i\n", read_size);
+
+			//Write the currently read data into our image file
+			write_size = fwrite(imagearray, 1, read_size, image);
+			printf("Written image size: %i\n", write_size);
+
+			if (read_size != write_size)
+			{
+				printf("error in read write\n");
+			}
+
+			//Increment the total number of bytes read
+			recv_size += read_size;
+			packet_index++;
+			printf("Total received image size: %i\n", recv_size);
+			printf(" \n");
+			printf(" \n");
+		}
+	}
+
+	fclose(image);
+	printf("Image successfully Received!\n");
+	return 1;
+}
+
+/**
+ * @brief Funcion encargada del logueo en el cliente. Se mantiene en un while hasta los 3 intentos de ingresar user-pass. Pasado dichos intentos, envia un FIN al cliente para indicar que se cierre.
+ * @author Kleiner Matias
+ * @param socket El descriptor del socket por donde se reciben los datos y se escribe la respuesta a los mismos.
+ * @date 30/03/2019
+ * @return 1 en caso correcto, @c 0 otherwise.
+ */
 int loggin(int sockfd)
 {
 	char buffer[TAM];
 	int terminar = 0;
-	int n;
-	inicio: memset(buffer, '\0', TAM);
-	printf(ANSI_COLOR_GREEN " USUARIO " ANSI_COLOR_RESET);
-	fgets(buffer, TAM - 1, stdin);
 
+	int n;
+
+	int user_or_pass = 0;
+
+inicio:
+	memset(buffer, '\0', TAM);
+	memset(hostname, '\0', sizeof(hostname));
+	char *name;
+	name = getlogin();
+
+	gethostname(hostname, sizeof(hostname));
+
+	printf("[");
+	printf(ANSI_COLOR_GREEN);
+	printf(name);
+	printf("@");
+	printf(hostname);
+	printf(ANSI_COLOR_RESET);
+	printf("]");
+	printf("$ ");
+	fflush(stdout);
+
+	fgets(buffer, TAM - 1, stdin);
 	n = write(sockfd, buffer, strlen(buffer));
 	if (n < 0)
 	{
@@ -87,6 +281,15 @@ int loggin(int sockfd)
 	{
 		terminar = 1;
 	}
+	if (user_or_pass == 0)
+	{
+		strcpy(user, buffer);
+		user_or_pass++;
+	}
+	else
+	{
+		user_or_pass = 0;
+	}
 
 	memset(buffer, '\0', TAM);
 	n = read(sockfd, buffer, TAM);
@@ -96,7 +299,7 @@ int loggin(int sockfd)
 		exit(1);
 	}
 
-	printf("Respuesta: %s\n", buffer);
+	//printf("Respuesta: %s\n", buffer);
 	if (strcmp(buffer, "FIN") == 0)
 	{
 		terminar = 1;
@@ -104,14 +307,19 @@ int loggin(int sockfd)
 
 	else if (strcmp(buffer, "SI") == 0)
 	{
+		n = write(sockfd, buffer, sizeof(buffer));
+		if (n < 0)
+		{
+			perror("escritura de socket");
+			exit(1);
+		}
 		return 1;
 	}
 
-	else {
+	else
+	{
 		goto inicio;
 	}
-
-
 
 	if (terminar)
 	{
--- TP1/codigo/servidor.c
+++ TP1/codigo/servidor.c
@@ -3,17 +3,16 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
-#define TAM 80
+#include <string.h>
+#define TAM 1024
 
-
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 struct Users
 {
@@ -24,23 +23,29 @@ struct Users
 struct Users arregloUsuarios[2];
 
 int autenticacion(int socket);
+int parse_func(int socket);
+int update_firmware(int sockfd);
 
 int main(int argc, char *argv[])
 {
 
 	int sockfd, newsockfd, servlen, clilen, n, pid;
-	int maquina_estado;
+	int control_error;
 	struct sockaddr_un cli_addr, serv_addr;
 	char buffer[TAM];
 
 	struct Users usuario1, usuario2;
 
-	strcpy(usuario1.id, "mati");
+	strcpy(usuario1.id, "matias");
 	strcpy(usuario1.pass, "ragnar");
 	strcpy(usuario2.id, "agus");
 	strcpy(usuario2.pass, "anij");
 	arregloUsuarios[0] = usuario1;
 	arregloUsuarios[1] = usuario2;
+
+	char temp[1024];
+
+	char hostname[1024];
 
 	/* Se toma el nombre del socket de la línea de comandos */
 	if (argc != 2)
@@ -94,45 +99,48 @@ int main(int argc, char *argv[])
 		{
 			// proceso hijo
 			close(sockfd);
-			while (1)
+
+			control_error = autenticacion(newsockfd);
+
+			if (control_error == 1)
 			{
-				memset(buffer, 0, TAM);
-				maquina_estado = autenticacion(newsockfd);
-
-				if (maquina_estado == 1)
-				{
-					printf("YUPIIIIIIIIIIII");
-					n = write(newsockfd, "SI", 3);
-					if (n < 0)
-					{
-						perror("escritura en socket");
-						exit(1);
-					}
-				}
-				else if (maquina_estado == 0)
-				{
-					printf("NOP");
-					exit(0);
-				}
-
-				printf("Recibí: %s \n", buffer);
-
-				n = write(newsockfd, "Obtuve su mensaje", 18);
+				printf("Envio user@host \n");
+				printf(temp);
+				printf("\n");
+				n = write(newsockfd, "SI", sizeof("SI"));
 				if (n < 0)
 				{
 					perror("escritura en socket");
 					exit(1);
 				}
-
-				// Verificación de si hay que terminar
-				if (!strcmp("fin", buffer))
+				n = read(newsockfd, buffer, TAM);
+				if (n < 0)
 				{
-					printf("PROCESO %d. Como recibí 'fin', termino la ejecución.\n\n", getpid());
-					exit(0);
+					perror("lectura de socket");
+					exit(1);
 				}
 
-				fflush(stdout);
-				strcpy(buffer, "");
+				strcat(temp, "@");
+				memset(hostname, 0, sizeof(hostname));
+				gethostname(hostname, sizeof(hostname));
+				strcat(temp, hostname);
+
+				n = write(newsockfd, temp, sizeof(temp));
+				if (n < 0)
+				{
+					perror("escritura en socket");
+					exit(1);
+				}
+			}
+			else if (control_error == 0)
+			{
+				printf("ERROR en autenticacion.");
+				exit(0);
+			}
+
+			while (1)
+			{
+				parse_func(newsockfd);
 			}
 		}
 		else
@@ -144,20 +152,29 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+/**
+ * @brief Funcion encargada del logueo en el servidor. Se mantiene en un while hasta los 3 intentos de ingresar user-pass. Pasado dichos intentos, envia un FIN al cliente para indicar que se cierre.
+ * @author Kleiner Matias
+ * @param socket El descriptor del socket por donde se reciben los datos y se escribe la respuesta a los mismos.
+ * @date 30/03/2019
+ * @return 1 en caso correcto, @c 0 otherwise.
+ */
 int autenticacion(int socket)
 {
+	//guardo id y usuario que llegan en estos buffer.
 	char autenticacion_id[TAM];
 	char autenticacion_pass[TAM];
-	char buffer[TAM];
-	int n, comparo_id_usuario, comparo_pass_usuario, contador_intentos;
 
-	contador_intentos = 1;
+	char buffer[TAM];
+	int control_errores, comparo_id_usuario, comparo_pass_usuario, contador_intentos;
+
+	contador_intentos = 1; // inicio en 1. Con cada intento de user-pass incremento. Cuando llego a 4, mando FIN y cierro cliente.
 
 	while (contador_intentos <= 4)
 	{
 		memset(buffer, '\0', TAM);
-		n = read(socket, buffer, TAM);
-		if (n < 0)
+		control_errores = read(socket, buffer, TAM);
+		if (control_errores < 0)
 		{
 			perror("lectura de socket");
 			exit(1);
@@ -166,16 +183,16 @@ int autenticacion(int socket)
 		strtok(buffer, "\n");
 		strcpy(autenticacion_id, buffer);
 
-		n = write(socket, "Obtuve su mensaje", 18);
-		if (n < 0)
+		control_errores = write(socket, "Obtuve su mensaje", sizeof("Obtuve su mensaje"));
+		if (control_errores < 0)
 		{
 			perror("escritura en socket");
 			exit(1);
 		}
 
 		memset(buffer, '\0', TAM);
-		n = read(socket, buffer, TAM);
-		if (n < 0)
+		control_errores = read(socket, buffer, TAM);
+		if (control_errores < 0)
 		{
 			perror("lectura de socket");
 			exit(1);
@@ -183,6 +200,7 @@ int autenticacion(int socket)
 		strtok(buffer, "\n");
 		strcpy(autenticacion_pass, buffer);
 
+		//recorro en un for los usuarios. Si encuentro alguno, uso el pass asignado para ese usuario y comparo con lo que recibi.
 		for (int i = 0; i < sizeof(arregloUsuarios); i++)
 		{
 			comparo_id_usuario = strcmp(arregloUsuarios[i].id, autenticacion_id);
@@ -191,6 +209,7 @@ int autenticacion(int socket)
 				comparo_pass_usuario = strcmp(arregloUsuarios[i].pass, autenticacion_pass);
 				if (comparo_pass_usuario == 0)
 				{
+					//retorno 1 en caso de coincidencia user-pass.
 					return 1;
 				}
 			}
@@ -200,22 +219,175 @@ int autenticacion(int socket)
 
 		if (contador_intentos == 4)
 		{
-			n = write(socket, "FIN", strlen("FIN"));
-			if (n < 0)
+			control_errores = write(socket, "FIN", sizeof("FIN"));
+			if (control_errores < 0)
 			{
 				perror("escritura en socket");
 				exit(0);
 			}
 		}
+
 		else
 		{
-			n = write(socket, "Obtuve su mensaje", 18);
-			if (n < 0)
+			control_errores = write(socket, "Obtuve su mensaje", sizeof("Obtuve su mensaje"));
+			if (control_errores < 0)
 			{
 				perror("escritura en socket");
 				exit(1);
 			}
 		}
 	}
+	return 0;
+}
+
+/**
+ * @brief Funcion encargada del logueo en el servidor. Se mantiene en un while hasta los 3 intentos de ingresar user-pass. Pasado dichos intentos, envia un FIN al cliente para indicar que se cierre.
+ * @author Kleiner Matias
+ * @param socket El descriptor del socket por donde se reciben los datos y se escribe la respuesta a los mismos.
+ * @date 30/03/2019
+ * @return 1 en caso correcto, @c 0 otherwise.
+ */
+int parse_func(int socket)
+{
+	//guardo id y usuario que llegan en estos buffer.
+
+	char buffer[TAM];
+	int control_errores;
+
+	memset(buffer, 0, TAM);
+	control_errores = read(socket, buffer, TAM);
+	if (control_errores < 0)
+	{
+		perror("lectura de socket");
+		exit(1);
+	}
+	printf("Recibí: %s \n", buffer);
+
+	strtok(buffer, "\n");
+	if (!strcmp("fin", buffer))
+	{
+		printf("PROCESO %d. Como recibí 'fin', termino la ejecución.\n\n", getpid());
+		exit(0);
+	}
+
+	if (strcmp(buffer, "update firmware.bin") == 0)
+	{
+		printf("update firmware.bin");
+		memset(buffer, '\0', TAM);
+		control_errores = write(socket, "recibi update", sizeof("recibi update"));
+		if (control_errores < 0)
+		{
+			perror("escritura en socket");
+			exit(1);
+		}
+		update_firmware(socket);
+	}
+	else if (strcmp(buffer, "start scanning") == 0)
+	{
+		printf("start scanning");
+		memset(buffer, '\0', TAM);
+		control_errores = write(socket, "recibi start scanning", sizeof("recibi start scanning"));
+		if (control_errores < 0)
+		{
+			perror("escritura en socket");
+			exit(1);
+		}
+	}
+	else if (strcmp(buffer, "obtener telemetrı́a") == 0)
+	{
+		printf("obtener telemetrı́a");
+		memset(buffer, '\0', TAM);
+		control_errores = write(socket, "recibi obtener telemetria", sizeof("recibi obtener telemetria"));
+		if (control_errores < 0)
+		{
+			perror("escritura en socket");
+			exit(1);
+		}
+	}
+	else
+	{
+		printf("incorrecto");
+		memset(buffer, '\0', TAM);
+		control_errores = write(socket, "incorrecto", sizeof("incorrecto"));
+		if (control_errores < 0)
+		{
+			perror("escritura en socket");
+			exit(1);
+		}
+	}
+
+	fflush(stdout);
+
+	return 0;
+}
+
+/**
+ * @brief Funcion encargada del logueo en el servidor. Se mantiene en un while hasta los 3 intentos de ingresar user-pass. Pasado dichos intentos, envia un FIN al cliente para indicar que se cierre.
+ * @author Kleiner Matias
+ * @param socket El descriptor del socket por donde se reciben los datos y se escribe la respuesta a los mismos.
+ * @date 30/03/2019
+ * @return 1 en caso correcto, @c 0 otherwise.
+ */
+int update_firmware(int socket)
+{
+
+	FILE *picture;
+	int size, read_size, stat, packet_index;
+	char send_buffer[TAM], read_buffer[TAM];
+	packet_index = 1;
+
+	picture = fopen("file.txt", "r");
+	printf("Getting Picture Size\n");
+
+	if (picture == NULL)
+	{
+		printf("Error Opening Image File");
+	}
+
+	fseek(picture, 0, SEEK_END);
+	size = ftell(picture);
+	fseek(picture, 0, SEEK_SET);
+	printf("Total Picture size: %i\n", size);
+	
+	
+	//Send Picture Size
+	printf("Sending Picture Size\n");
+	usleep(1000);
+	write(socket, &size, sizeof(size));
+	
+	//Send Picture as Byte Array
+	printf("Sending Picture as Byte Array\n");
+
+	do
+	{ //Read while we get errors that are due to signals.
+		stat = read(socket, &read_buffer, 255);
+		printf("Bytes read: %i\n", stat);
+	} while (stat < 0);
+
+	printf("Received data in socket\n");
+
+	while (!feof(picture))
+	{
+		//while(packet_index = 1){
+		//Read from the file into our send buffer
+		read_size = fread(send_buffer, 1, sizeof(send_buffer) - 1, picture);
+
+		//Send data through our socket
+		do
+		{
+			stat = write(socket, send_buffer, read_size);
+		} while (stat < 0);
+
+		printf("Packet Number: %i\n", packet_index);
+		printf("Packet Size Sent: %i\n", read_size);
+		printf(" \n");
+		printf(" \n");
+
+		packet_index++;
+
+		//Zero out our send buffer
+		bzero(send_buffer, sizeof(send_buffer));
+	}
+
 	return 0;
 }
