9987ba4bd6efaa005676dffa053e945c5b9c47d0
ragnar-l <kleiner.matias@gmail.com>
2019-05-10 21:31:40 -0300

    convolve

--- TP2/convolve.c
+++ TP2/convolve.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <netcdf.h>
-#include <omp.h>
 #include <time.h>
+#include <omp.h>
+
 
 /* Lectura de una matriz de 21696 x 21696 */
 
@@ -27,6 +28,7 @@ int main()
 {
     float *data_out = (float*)malloc(NX * NY * sizeof(float));
     float *data_in = (float*)malloc(NX * NY * sizeof(float));
+    
     float matriz_w[WX][WY] =
         {
             {-1, -1, -1}, /*  initializers for row indexed by 0 */
@@ -52,10 +54,7 @@ int main()
     if ((retval = nc_inq_varid(ncid, "CMI", &varid)))
         ERR(retval);
 
-    //for (j = 1; j < NX - 1; j++)
-    //{
-
-    //}
+    
     if ((retval = nc_get_vara_float(ncid, varid, start, conteo, data_in)))
         ERR(retval);
 
@@ -63,34 +62,12 @@ int main()
         ERR(retval);
 
     nc_close(ncid);
-
-    /**
-    printf("Matriz inicial: \n ");
-    for (int i = 0; i < NX; i++)
-    {
-        for (int j = 0; j < NX; j++)
-        {
-            printf("%lf ", data_in[i][j]);
-        }
-        printf("%lf",(float)(0.0 / 0.0));
-    }
-    return (0);
-    **/
-
-    //printf("%lf",(float)(0.0 / 0.0)*10);
-    //return (0);
-   
-    convolve(data_in, data_out, matriz_w);
-    free(data_in);
-    
-
     for (int j = 1; j < NX - 1; j++)
     {
         for (int i = 1; i < NX - 1; i++)
         {
             if (data_out[(i - 1) + ((j - 1) * NX)] == (float)(0.0/0.0))
             {
-                printf("Matriz inicial: \n ");
                 data_out[(i - 1) + ((j - 1) * NX)] = (0);
             }
 
@@ -135,6 +112,10 @@ int main()
             }
         }
     }
+   
+    convolve(data_in, data_out, matriz_w);
+    free(data_in);
+    
 
     escribir_nc(data_out, 0, 0);
     free(data_out);
@@ -144,31 +125,34 @@ int main()
 
 void convolve(float *data_in, float *data_out, float c[][WY])
 {
-    clock_t ini,fin;
-
-    double segundos;
-
-    ini=clock();
 
     int j, i;
-    #pragma omp parallel for collapse(2) num_threads(4) 
+    struct timespec start, end;
     
+    if( clock_gettime( CLOCK_MONOTONIC_RAW, &start) == -1 ) {
+      perror( "clock gettime" );
+      exit( EXIT_FAILURE );
+    }
+    
+    #pragma omp parallel for num_threads(4) 
     for (j = 1; j < NX - 1; j++)
     {
         for (i = 1; i < NX - 1; i++)
         {
-            
-
-            data_out[(i) + (j * NX)] = (data_in[(i - 1) + ((j - 1) * NX)] * c[0][0]) + (data_in[(i) + ((j - 1) * NX)] * c[0][1]) + (data_in[(i + 1) + ((j - 1) * NX)] * c[0][2]);
-            data_out[(i) + (j * NX)] = data_out[(i) + (j * NX)] + (data_in[(i - 1) + ((j)*NX)] * c[1][0]) + (data_in[(i) + ((j)*NX)] * c[1][1]) + (data_in[(i + 1) + ((j)*NX)] * c[1][2]);
-            data_out[(i) + (j * NX)] = data_out[(i) + (j * NX)] + (data_in[(i - 1) + ((j + 1) * NX)] * c[2][0]) + (data_in[(i) + ((j + 1) * NX)] * c[2][1]) + (data_in[(i + 1) + ((j + 1) * NX)] * c[2][2]);
-            data_out[(i) + (j * NX)] = data_out[(i) + (j * NX)] *  (float)(0.00031746);
+            data_out[(i) + (j * NX)] = ((data_in[(i - 1) + ((j - 1) * NX)] * c[0][0]) + (data_in[(i) + ((j - 1) * NX)] * c[0][1]) + (data_in[(i + 1) + ((j - 1) * NX)] * c[0][2]) + (data_in[(i - 1) + ((j)*NX)] * c[1][0]) + (data_in[(i) + ((j)*NX)] * c[1][1]) + (data_in[(i + 1) + ((j)*NX)] * c[1][2]) + (data_in[(i - 1) + ((j + 1) * NX)] * c[2][0]) + (data_in[(i) + ((j + 1) * NX)] * c[2][1]) + (data_in[(i + 1) + ((j + 1) * NX)] * c[2][2]))*(float)(0.00031746);
         }
     }
 
-    fin=clock();
-    segundos=(double)(fin-ini)/CLOCKS_PER_SEC;
-    printf("%.16g \n", segundos);
+    
+    if( clock_gettime( CLOCK_MONOTONIC_RAW, &end) == -1 ) {
+      perror( "clock gettime" );
+      exit( EXIT_FAILURE );
+    }
+
+   u_int64_t delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+   u_int64_t total_time_s = delta_us/1000000;
+   u_int64_t total_time_ms = (delta_us%1000000)/1000;
+   printf("TERMINO DESCARGA EN %lds %ldms %ldus\n",total_time_s, total_time_ms, delta_us%1000);
 }
 
 int escribir_nc(float *data_out, int punterox, int punteroy)
