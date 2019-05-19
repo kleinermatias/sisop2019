82e4a1a494c5a05fa5eb62c5cd5fe1dbed54f678
matias <kleiner.matias@gmail.com>
2019-05-19 21:53:58 +0100

    navbar

---
+++ TP3/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+
+    document.getElementById("navbar-frame").innerHTML = '<div id="navbar-frame">' +  
+    '<nav class="navbar navbar-dark bg-primary">' +
+     '   <div class="container">' +
+     '    <a class="navbar-brand" href="/sisop2019/TP3/index.lighttpd.html">TP2 - SO2 2019</a>'+
+     '    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">'+
+     '      <span class="navbar-toggler-icon"></span>'+
+     '    </button>'+
+     '    <div class="collapse navbar-collapse" id="navbarResponsive">'+
+     '      <ul class="navbar-nav ml-auto">'+
+     '        <li class="nav-item active">'+
+     '          <a class="nav-link" href="/sisop2019/TP3/index.lighttpd.html">Info'+
+     '            <span class="sr-only">(current)</span>'+
+     '          </a>'+
+     '        </li>'+
+     '        <li class="nav-item">'+
+     '          <a class="nav-link" href="/sisop2019/TP3/formulario.html">Formulario</a>'+
+     '        </li>'+
+     '        <li class="nav-item">'+
+     '          <a class="nav-link" href="/sisop2019/TP3/modulos.html">Modulos instalados</a>'+
+     '        </li>'+
+     '      </ul>'+
+     '    </div>'+
+     '  </div>'+
+     ' </nav>'+
+     '</div>'
+    
+    
+    });
